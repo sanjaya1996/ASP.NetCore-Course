152207d15b79e255642afa5d709865cc4e52c890
Sanzay Dahal <sanzaydahal12345@gmail.com>
2024-07-20 18:31:59 +1000

    Partial Views - PartialViewsExample project

---
+++ PartialViewsExample/PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsExample.Models;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("programming-languages")]
+        public IActionResult ProgrammingLanguages()
+        {
+            ListModel listModel = new ListModel()
+            {
+                ListTitle = "Programming Languages List",
+                ListItems = new List<string>()
+                {
+                    "Java", "C#", "Python", "C++", "JavaScript"
+                }
+        };
+            return PartialView("_ListPartialView", listModel);
+        }
+    }
+}
