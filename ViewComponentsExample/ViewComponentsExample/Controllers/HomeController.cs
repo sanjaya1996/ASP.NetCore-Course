c9b711194bfe95691bfce860f8dba17b17db17f6
Sanzay Dahal <sanzaydahal12345@gmail.com>
2024-07-20 23:08:11 +1000

    View Componets - ViewComponentsExample project

---
+++ ViewComponentsExample/ViewComponentsExample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace ViewComponentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("friends-list")]
+        public IActionResult LoadFriendsList()
+        {
+            return ViewComponent("Grid", new { gridType = "friends" });
+        }
+    }
+}
---
+++ ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
+
+namespace ViewComponentsExample.ViewComponents
+{
+    public class GridViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string? gridType)
+        {
+            {
+
+                PersonGridModel personGridModel = new PersonGridModel()
+                {
+                    GridTitle = "Persons List",
+                    Persons = new List<Person>
+                    {
+                        new Person(){ PersonName = "John", JobTitle = "Manager"},
+                        new Person(){ PersonName = "Sanjay", JobTitle = "Manager"},
+                        new Person(){ PersonName = "William", JobTitle = "Clerk"}
+
+                    }
+                };
+
+                if(gridType == "friends")
+                {
+                    personGridModel = new PersonGridModel()
+                    {
+                        GridTitle = "Friend List",
+                        Persons = new List<Person>
+                    {
+                        new Person(){ PersonName = "Bijay", JobTitle = "Software Developer"},
+                        new Person(){ PersonName = "Sam", JobTitle = "Engineering Manager"},
+                        new Person(){ PersonName = "John", JobTitle = "Front-end Developer"}
+
+                    }
+                    };
+                }
+
+                return View("Sample", personGridModel); // invoked a partial view Views/Shared/Components/Grid/{Default | Sample}.cshtml
+
+            }
+        }
+    }
+    }
