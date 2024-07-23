722711d90089c56d89f1aab8a104ef9a65a40e40
Sanzay Dahal <sanzaydahal12345@gmail.com>
2024-07-24 01:17:10 +1000

    Working with Environment - EnvironmentsExample project

---
+++ EnvironmentsExample/EnvironmentsExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+           ViewBag.CurrentEnvironment = _webHostEnvironment.EnvironmentName;
+            return View();
+        }
+    }
+}
